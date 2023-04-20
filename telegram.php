5d4a0031046f4087761731c6fd4bd259b152e216
starvingartistM <131155959+starvingartistM@users.noreply.github.com>
2023-04-20 20:42:09 +0400

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -22,7 +22,7 @@ foreach($arr as $key => $value) {
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-  header('Спасибо!');
+  header('Location: index.html');
 } else {
   echo "Error";
 }
